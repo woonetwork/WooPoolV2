42b6860030e61e7ee6d5a7a124ae0e48336a5096
Merlin <39942523+merlincdj@users.noreply.github.com>
2022-09-17 10:59:03 +0800

    chore: add deploy scripts (#4)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -14,6 +14,8 @@ task("accounts", "Prints the list of accounts", async (_args, hre) => {
   accounts.forEach(async (account) => console.info(account.address));
 });
 
+const accounts = process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [];
+
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   networks: {
@@ -26,9 +28,76 @@ const config: HardhatUserConfig = {
       },
       gasPrice: "auto",
     },
+    bsc_mainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      accounts: accounts,
+    },
+    bsc_testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+      chainId: 97,
+      accounts: accounts,
+    },
+    avalanche_mainnet: {
+      url: "https://api.avax.network/ext/bc/C/rpc/",
+      chainId: 43114,
+      accounts: accounts,
+    },
+    avalanche_testnet: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc/",
+      chainId: 43113,
+      accounts: accounts,
+    },
+    fantom_mainnet: {
+      url: "https://rpc.ftm.tools/",
+      chainId: 250,
+      accounts: accounts,
+    },
+    fantom_testnet: {
+      url: "https://rpc.testnet.fantom.network/",
+      chainId: 4002,
+      accounts: accounts,
+    },
+    polygon_mainnet: {
+      url: "https://polygon-rpc.com/",
+      chainId: 137,
+      accounts: accounts,
+    },
+    polygon_testnet: {
+      url: "https://matic-mumbai.chainstacklabs.com/",
+      chainId: 80001,
+      accounts: accounts,
+    },
+    arbitrum_mainnet: {
+      url: "https://arb1.arbitrum.io/rpc/",
+      chainId: 42161,
+      accounts: accounts,
+    },
+    arbitrum_testnet: {
+      url: "https://rinkeby.arbitrum.io/rpc/",
+      chainId: 421611,
+      accounts: accounts,
+    },
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_KEY,
+    apiKey: {
+      mainnet: process.env.ETHERSCAN_KEY,
+      // binance smart chain
+      bsc: process.env.BSCSCAN_KEY,
+      bscTestnet: process.env.BSCSCAN_KEY,
+      // avalanche
+      avalanche: process.env.SNOWTRACE_KEY,
+      avalancheFujiTestnet: process.env.SNOWTRACE_KEY,
+      // fantom mainnet
+      opera: process.env.FTMSCAN_KEY,
+      ftmTestnet: process.env.FTMSCAN_KEY,
+      // polygon
+      polygon: process.env.POLYGONSCAN_KEY,
+      polygonMumbai: process.env.POLYGONSCAN_KEY,
+      // arbitrum
+      arbitrumOne: process.env.ARBISCAN_KEY,
+      arbitrumTestnet: process.env.ARBISCAN_KEY,
+    },
   },
   solidity: {
     compilers: [
---
+++ scripts/WooPPV2.ts
@@ -0,0 +1,35 @@
+// eslint-disable-next-line node/no-unpublished-import
+import { ethers, run } from "hardhat";
+
+// eslint-disable-next-line prefer-const
+let contractName = "WooPPV2";
+
+// Specify need before deploying contract
+const quoteToken = "";
+
+async function main() {
+  const args = [quoteToken];
+  const factory = await ethers.getContractFactory(contractName);
+  const contract = await factory.deploy(...args);
+  await contract.deployed();
+  console.log(`${contractName} deployed to: ${contract.address}`);
+
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+  try {
+    await run("verify:verify", {
+      address: contract.address,
+      constructorArguments: args,
+    });
+  } catch (e) {
+    if (typeof e === "string") {
+      console.log(e.toUpperCase()); // works, `e` narrowed to string
+    } else if (e instanceof Error) {
+      console.log(e.message); // works, `e` narrowed to Error
+    }
+  }
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
