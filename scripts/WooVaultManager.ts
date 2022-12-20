d6dcc76d004c66fe6161e0fc3053697be0a75d12
merlincdj <377730858@qq.com>
2022-12-20 19:21:13 +0800

    chore: update addresses

--- scripts/WooVaultManager.ts
+++ scripts/WooVaultManager.ts
@@ -13,20 +13,15 @@ const owner = "0x7C8A5d20b22Ce9b369C043A3E0091b5575B732d9";
 async function main() {
   const args = [quoteToken, rewardToken, wooAccessManager];
   const factory = await ethers.getContractFactory(contractName);
-  // const contract = await factory.deploy(...args);
-  // await contract.deployed();
-  // console.log(`${contractName} deployed to: ${contract.address}`);
-
-  const contract = factory.attach("0x88748243DE01c4F3C103F2De2833f39F6807db17");
+  const contract = await factory.deploy(...args);
+  await contract.deployed();
+  console.log(`${contractName} deployed to: ${contract.address}`);
 
   // await contract.setWooRouter(wooRouterV2); // Set WooRouterV2 manually
   // await contract.setVaultWeight(stakingVault, 100); // Set vault weight manually
 
-  await contract.setWooRouter("0x817Eb46D60762442Da3D931Ff51a30334CA39B74"); // Set WooRouterV2 manually
-  await contract.setVaultWeight("0x9BCf8b0B62F220f3900e2dc42dEB85C3f79b405B", 100); // Set vault weight manually
-
   await new Promise((resolve) => setTimeout(resolve, 10000));
-  // await contract.transferOwnership(owner);
+  await contract.transferOwnership(owner);
   
   try {
     await run("verify:verify", {
