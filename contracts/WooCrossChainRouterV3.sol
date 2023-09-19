b60429909fe6de1fe2eeff67f79c64fecda38a5b
QCQC <hackerqc@gmail.com>
2023-09-19 18:57:00 +0800

    [For you reference] update BASE and add pausable  for cross chain router. (#85)
    * feat: Fix BASE in WooCrossChainRouterV3
    
    * feat: Support pausable in WooCrossChainRouterV3

--- contracts/WooCrossChainRouterV3.sol
+++ contracts/WooCrossChainRouterV3.sol
@@ -4,6 +4,7 @@ pragma solidity =0.8.14;
 // OpenZeppelin Contracts
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Pausable} from "@openzeppelin/contracts/security/Pausable.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
@@ -20,7 +21,7 @@ import {TransferHelper} from "./libraries/TransferHelper.sol";
 /// @title cross chain router implementation, version 3.
 /// @notice Router for stateless execution of cross chain swap against WOOFi or 1inch swap.
 /// @custom:stargate-contracts https://stargateprotocol.gitbook.io/stargate/developers/contract-addresses/mainnet
-contract WooCrossChainRouterV3 is IWooCrossChainRouterV3, Ownable, ReentrancyGuard {
+contract WooCrossChainRouterV3 is IWooCrossChainRouterV3, Ownable, Pausable, ReentrancyGuard {
     using EnumerableSet for EnumerableSet.AddressSet;
 
     /* ----- Constants ----- */
@@ -41,7 +42,7 @@ contract WooCrossChainRouterV3 is IWooCrossChainRouterV3, Ownable, ReentrancyGua
     uint16 public sgChainIdLocal; // Stargate chainId on local chain
     uint16 public srcExternalFeeRate; // unit: 0.1 bps (1e6 = 100%, 25 = 2.5 bps)
     uint16 public dstExternalFeeRate; // unit: 0.1 bps (1e6 = 100%, 25 = 2.5 bps)
-    uint256 public constant FEE_BASE = 1e6;
+    uint256 public constant FEE_BASE = 1e5;
 
     mapping(uint16 => address) public wooCrossRouters; // chainId => WooCrossChainRouterV3 address
     mapping(uint16 => address) public sgETHs; // chainId => SGETH token address
@@ -133,7 +134,7 @@ contract WooCrossChainRouterV3 is IWooCrossChainRouterV3, Ownable, ReentrancyGua
         DstInfos calldata dstInfos,
         Src1inch calldata src1inch,
         Dst1inch calldata dst1inch
-    ) external payable nonReentrant {
+    ) external payable whenNotPaused nonReentrant {
         require(srcInfos.fromToken != address(0), "WooCrossChainRouterV3: !srcInfos.fromToken");
         require(
             dstInfos.toToken != address(0) && dstInfos.toToken != sgETHs[dstInfos.chainId],
@@ -639,6 +640,14 @@ contract WooCrossChainRouterV3 is IWooCrossChainRouterV3, Ownable, ReentrancyGua
     function removeDirectBridgeToken(address token) external onlyOwner {
         bool success = directBridgeTokens.remove(token);
         require(success, "WooCrossChainRouterV3: token not exist");
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
     }
 
     function inCaseTokenGotStuck(address stuckToken) external onlyOwner {
